9afc81117d8ea1f011c42758d06f1d2a96bfe6c1
Gianlucas Bertone <lucascorrado23@gmail.com>
2025-02-18 19:05:37 +0000

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(num + '0');
-	if (num < 9)
+		putchar(num);
+		if (num != '9')
 	{
 		putchar(',');
 		putchar(' ');
